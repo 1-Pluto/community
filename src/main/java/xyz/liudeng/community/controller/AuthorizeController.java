5b6ec831d573dc22bdc5359e829c71c537c0f0cb
liudeng <1137480047@qq.com>
2019-08-15 16:27:48 +0800

    登录完成  准备H2数据库

--- src/main/java/xyz/liudeng/community/controller/AuthorizeController.java
+++ src/main/java/xyz/liudeng/community/controller/AuthorizeController.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.misc.Request;
 import xyz.liudeng.community.dto.AccessTokendDTO;
 import xyz.liudeng.community.dto.GithubUser;
 import xyz.liudeng.community.provider.GithubProvider;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author liudeng
@@ -29,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")  String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokendDTO accessTokendDTO=new AccessTokendDTO();
         accessTokendDTO.setRedirect_uri(redirectUri);
         accessTokendDTO.setCode(code);
@@ -38,7 +42,13 @@ public class AuthorizeController {
         accessTokendDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokendDTO);
         GithubUser user = githubProvider.getuser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user!=null){
+            //登录成功    写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登录失败  重新登陆
+            return "redirect:/";
+        }
     }
 }
